0ff7a4be8178a992884422aaadcef215a47e0f10
Gonzalo Alava <gonzaalava@gmail.com>
2021-03-22 20:05:43 -0300

    Correccion Entrega 1

--- FC+AlavaGonzalo.js
+++ FC+AlavaGonzalo.js
@@ -2,16 +2,20 @@ function Usuario(nombre, apellido, libro, mascota) {
 	this.nombre = nombre;
 	this.apellido = apellido;
 	this.libro = libro;
+    this.autor = autor;
 	this.mascota = mascota;
 	
 	this.getFullName = function(){return this.nombre + this.apellido};
-    this.addMascota = function(mascota){this.mascota + 1};
+    this.addMascota = function(){this.mascota + 1};
     this.getMascota = function(){return mascota};
-};
+    this.getBookAutor = function(){return autor};
+   }       
+}
 
-let usuario = new Usuario("Raul", "Bernardino", "El libro de los abrazos", 2);
+let usuario = new Usuario("Raul", "Bernardino", "Ser como ellos", "Eduardo Galeano", 2);
 
 console.log(usuario.getFullName());
 
 console.log(usuario.getMascota());
 
+console.log(usuario.getBookAutor());
